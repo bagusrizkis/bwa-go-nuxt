1d419fe5be60787f5bb01246207df82c847b68ef
bagusrizkis <bagus.tho@gmail.com>
2021-08-15 14:46:31 +0700

    create campaign endpoint

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-playground/validator/v10 v10.8.0
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gosimple/slug v1.10.0
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
--- handler/campaign.go
+++ handler/campaign.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"bwastartup/campaign"
 	"bwastartup/helper"
+	"bwastartup/user"
 	"net/http"
 	"strconv"
 
@@ -62,5 +63,38 @@ func (h *campaignHandler) GetCampaign(c *gin.Context) {
 	}
 
 	response := helper.APIResponse("Campaign Detail", http.StatusOK, "success", campaign.FormatDetailCampaign(campaignDetail))
+	c.JSON(http.StatusOK, response)
+}
+
+// tangkap parameter dari user ke input struct
+// ambil current user dari JWT/handler
+// panggil service, parameter input struct (dan buat slug)
+// panggil repository untuk simpan data campaign baru
+
+func (h *campaignHandler) CreateCampaign(c *gin.Context) {
+	var input campaign.CreateCampaignInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failled to create campaign", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+
+	input.User = currentUser
+
+	newCamapaign, err := h.service.CreateCampaign(input)
+	if err != nil {
+		response := helper.APIResponse("Failled to create campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success to create campaign", http.StatusOK, "success", campaign.FormatCampaign(newCamapaign))
 	c.JSON(http.StatusOK, response)
 }
